840ede987ede94d7032c769b071ecbfdbcd4a35e
juicio8 <you@example.com>
2022-07-01 16:44:55 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphab;
+
+	alphab = 'a';
+
+	do 
+	{
+		if (alphab == 'e' || alphab == 'q')
+		{
+			alphab++;
+			continue;
+		}
+		putchar(alphab);
+		alphab++;
+
+	} while (alphab <= 'z');
+	putchar('\n');
+	return (0);
+}
