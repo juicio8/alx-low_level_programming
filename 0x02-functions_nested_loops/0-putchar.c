e77085285046cfb90150f1d07af63f90366dc76d
juicio8 <you@example.com>
2022-07-05 08:47:20 -0700

    00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,29 +1,10 @@
-#include <stdio.h>
-/**
+#include <unistd.h>
+/**i
  * _putchar - display _putchar
  *
  * Return: Always 0 (Success)
  */
-int _putchar(void)
+int _putchar(char c)
 }
-	int word;
-
-	word = '_';
-	putchar(word);
-	word = 'p';
-	putchar(word);
-	word = 'u';
-	putchar(word);
-	word = 't';
-	putchar(word);
-	word = 'c';
-	putchar(word);
-	word = 'h';
-	putchar(word);
-	word = 'a';
-	putchar(word);
-	word = 'r';
-	putchar(word);
-	putchar('\n');
-	return (0);
+	return(write(1, &c, 1));
 }
