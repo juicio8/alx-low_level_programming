cc9e1f30b9d8e8525cbe0d3e180a568bc2db7bd7
juicio8 <you@example.com>
2022-07-05 13:00:04 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_last_digit - print last digit of a number
+ *
+ * Return: Always return number
+ */
+int print_last_digit(int);
+{
+       	c;
+	return(c % 10);
+}
