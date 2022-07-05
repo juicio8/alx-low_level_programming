1ad9bf2df2ab26380730438392f1cc7d4c32be62
juicio8 <you@example.com>
2022-07-05 12:29:21 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * abs - display absolute number
+ * @int: The integer to be computed
+ * Return: The absolute value of the integer
+ */
+int _abs(int)
+{
+	if (int < 0 || int < 0)
+		return (int);
+	else
+		return (0);
+}
