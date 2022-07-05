ec9e812aa46596e389cbbfb494d49f11cee804dd
juicio8 <you@example.com>
2022-07-05 09:55:57 -0700

    03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
-#include <conio.h>
+/**
+ * int _islower - check if letter is lower
+ * @c: character to check
+ * Return: 0 (Success)
+ */
+int  _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
