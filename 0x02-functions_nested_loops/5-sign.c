ad989bc41aac70f0816142638902a3b11043d286
juicio8 <you@example.com>
2022-07-05 11:30:19 -0700

    05

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		return (1);
+		return (write('+', 1));
 	}
 	else if (n < 0)
 	{
-		putchar('-');
-		return (-1);
+		return (write('-', -1));
 	}
 	else
 	{
-		putchar('0' + 0);
-		return (0);
+		return (write('0' + 0, 0));
 	}
 }
 
