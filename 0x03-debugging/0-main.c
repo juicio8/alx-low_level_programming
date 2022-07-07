26bb2e7211fca84f9913c490e976b9a3c4c71e24
juicio8 <you@example.com>
2022-07-07 11:45:53 -0700

    o1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
+
+int main()
 {
 	int i;
 
 	i = 0;
-	return (positive_or_negative(i));
+
+	positive_or_negative(0);
+	
 }
