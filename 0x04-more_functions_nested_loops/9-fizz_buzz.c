19d8665c181377572086ba348716a8db07506f86
juicio8 <you@example.com>
2022-07-07 05:25:17 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,16 +16,22 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == 100);
+		if (i == 100)
 			printf("%s", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s ", fb);
 		else if (i % 3 == 0)
+		{
 			printf("%s ", f);
+		}
 		else if (i % 5 == 0)
+		{
 			printf("%s ", b);
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
