f9acbf0bd3abfba89b6a8d7d42fb487f1e92ec32
juicio8 <you@example.com>
2022-07-08 03:11:05 -0700

    00

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,8 +20,12 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if ((largest = a) && (largest = b))
+	else if ((a == b) && (a > c))
 		largest = a;
+	else if ((a == c) && (a > b))
+		largest = a;
+	else if ((b == c) && (b > a))
+		largest = b;
 	else
 	{
 		largest = c;
