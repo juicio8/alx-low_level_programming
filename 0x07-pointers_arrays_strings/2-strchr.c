12a78cf8bf801f437f70724211b01c6cab2762ce
juicio8 <you@example.com>
2022-07-18 08:54:21 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _stchr - locate character in string
+ * @s: source string
+ * @c: character to find
+ * Return: the string from character found
+ */
+char *_stchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+	return ('\0');
+}
