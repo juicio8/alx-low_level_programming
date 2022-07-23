58705654edb3a89f1ad31574a1b54c29c87c56ed
juicio8 <you@example.com>
2022-07-23 12:49:46 -0700

    a program to print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,11 +5,13 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
