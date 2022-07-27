37b28f88d6c8a03756b67a0245baaaaba2a32d23
juicio8 <you@example.com>
2022-07-26 21:01:28 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - free grids
+ * @grid: grid to be considered
+ * @height: row of height
+ * Return: 0
+ */
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = 0; i < height, i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
