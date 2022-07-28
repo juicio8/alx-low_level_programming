c7ab96f834b58d91dc8c24a4bf3236b02750f4c7
juicio8 <you@example.com>
2022-07-28 15:33:41 -0700

    creates int arrays

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+ * array_range - ...
+ * @min: min num
+ * @max: max num
+ * Return: integer value
+ */
+int *array_range(int min, int max)
+{
+	int *a, i = 0;
+
+	if (min > max)
+		return (NULL);
+	a = malloc((sizeof(int) * (max - min) + sizeof(int));
+	if (a == NULL)
+		return (NULL);
+	while (min <= max)
+	{
+		a[i] = min;
+		i++;
+		min++;
+	}
+	return (a);
+
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
+nt *array_range(int min, int max);
 
 #endif
 
