0ab79d83a31425ee15bb3afb43d3a22eff800a78
juicio8 <you@example.com>
2022-08-01 10:55:22 -0700

    create a function for initializing struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * init_dog - initializes a dog structure
+ * @d: A dog structure.
+ * @name: name of dog.
+ * @age: age of the dog.
+ * @owner: The owner of the dog.
+ * Return: Nothing
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
