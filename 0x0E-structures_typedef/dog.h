093aac92414756e8691751119df718926abe9524
juicio8 <you@example.com>
2022-08-01 10:45:54 -0700

    define a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
