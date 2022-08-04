b6738939d6b3306d061e2704a698da3322120954
juicio8 <you@example.com>
2022-08-04 09:26:16 -0700

    return sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,17 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 
 	va_start(ap, n);
-	for (i = 0; i < n; i++)
+
+	if (n == 0)
 	{
-		sum += va_arg (ap, int);
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(ap, int);
+		}
 	}
 	va_end(ap);
 	return (sum);
