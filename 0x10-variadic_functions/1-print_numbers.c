d2028c2a602493be3d90742c7fef7b3f82702195
juicio8 <you@example.com>
2022-08-04 11:40:41 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
-		printf("\n");
-		va_end(args);
 	}
+	printf("\n");
+	va_end(args);
+	
 }
