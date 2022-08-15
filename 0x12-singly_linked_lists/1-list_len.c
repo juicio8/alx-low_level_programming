f7dbfd7f75185178e1d2dab9f58c8104b479cbf8
juicio8 <you@example.com>
2022-08-15 07:42:11 -0700

    count elements of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len = returns no of elements in a list
+ * @h: pointer to list head
+ * Return: no of elements
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int count = 0;
+
+	tmp = h;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
+}
