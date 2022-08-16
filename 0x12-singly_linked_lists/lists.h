4dcbea66bdf5e128462f2729d66d4ad4670d2346
juicio8 <you@example.com>
2022-08-16 04:27:37 -0700

    add node to beginning

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,7 +14,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
-
+#include <stddef.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 unsigned int _strlen(char *str);
