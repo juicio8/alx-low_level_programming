0a8de838a1bc18d54562a8fff91073b18d22e22c
juicio8 <you@example.com>
2022-08-16 05:11:43 -0700

    free lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_list - frees a list_t list 
+ * free_list - free list
  * @head: pointer to the start of the list
- *
  * Return: void
  */
 void free_list(list_t *head)
