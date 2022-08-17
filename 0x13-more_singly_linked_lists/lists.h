2d8f59f5d3013e39142abf694aaf7794a4a1e363
juicio8 <you@example.com>
2022-08-17 13:36:30 -0700

    frees a list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef _LISTS_H_
-#define _LISTS_H
+#define _LISTS_H_
 #include <stddef.h>
 /**
  * struct listint_s - singly linked list
@@ -10,8 +10,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
