920274123ff07e0b29f2653ce6e2e6e3fd7e91ef
juicio8 <you@example.com>
2022-08-18 11:34:07 -0700

    first commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,9 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1)
 			return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
 }
