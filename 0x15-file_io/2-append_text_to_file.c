502795cd25ce727399d3b64573404f772b05a976
juicio8 <you@example.com>
2022-08-23 03:01:27 -0700

    äppend a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,18 +9,18 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t len;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
-	{
-		if (write(fd, text_content, _strlen(text_content)) == -1);
-			return (-1);
-	}
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
+	if (len == -1)
+		return (-1);
 	return (1);
 }
 
