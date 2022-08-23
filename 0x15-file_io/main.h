e771821e406ba5a3c2dddf48808270d1bac315a1
juicio8 <you@example.com>
2022-08-23 02:19:44 -0700

    create file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
