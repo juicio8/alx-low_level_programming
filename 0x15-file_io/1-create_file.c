814c19b4f8c85b7d3cb72a3d633777ae024f8ccf
juicio8 <you@example.com>
2022-08-23 02:16:47 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 		write(fd, text_content, _strlen(text_content));
-
 	close(fd);
-	return(1);
+	return (1);
 
 }
 /**
@@ -29,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 int _strlen(char *s)
 {
 	int c = 0;
-	
+
 	while (s[c])
 		c++;
 	return (c);
