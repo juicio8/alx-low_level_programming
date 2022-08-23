fa0116310c32a0c9e133483cc89e3830e4560264
juicio8 <you@example.com>
2022-08-23 02:15:17 -0700

    create file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,4 +6,6 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(char *s);
+int create_file(const char *filename, char *text_content);
 #endif
