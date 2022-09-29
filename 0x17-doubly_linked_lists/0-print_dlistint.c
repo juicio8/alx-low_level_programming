b7c731b897345d21a00a05dde41fada17c77d53b
juicio8 <juicio8@gmail.com>
2022-09-29 08:58:59 -0700

    initial commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - print all elements of a dlistint_t list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: no of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; i != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
