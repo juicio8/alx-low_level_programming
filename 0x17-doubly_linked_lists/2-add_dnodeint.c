34f5d6b2c594564424b6126e1fdcace924cbdd8b
juicio8 <juicio8@gmail.com>
2022-09-29 09:42:38 -0700

    2 fixed error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
-	new = malloc(sizeof(dlistint_t);
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return (NULL)
+		return (NULL);
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
