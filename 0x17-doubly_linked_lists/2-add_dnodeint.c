049c5eef9e4e037a0c732f841a7389b7327fe66a
juicio8 <juicio8@gmail.com>
2022-09-29 09:36:22 -0700

    initial commit

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds node at the beg on a list
+ * @head: starting address
+ * @n: value to add to new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t);
+	if (new == NULL)
+		return (NULL)
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
+}
